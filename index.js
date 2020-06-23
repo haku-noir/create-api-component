3742929780852747c013bd1fbc34b973ad1b6ea0
haku-noir <hakunoir.00@gmail.com>
2020-06-23 16:44:18 +0900

    add: srcディレクトリを探す関数の追加

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const fs = require('fs-extra');
+
+const getSrcDir = dir => {
+  let srcDir = `${dir}/src`;
+
+  dir.split('/').forEach((dirItem, index) => {
+    if(dirItem === 'src')
+      srcDir = dir.split('/').slice(0, index).join('/') + '/src';
+  });
+
+  return srcDir;
+};
+
 module.exports = () => {
+  const srcDir = getSrcDir(process.cwd());
+
   Promise.resolve()
-   .then(() => console.log('create-api-component'))
+   .then(() => console.log(srcDir))
    .catch(err => {
      console.log('Fail!');
      console.error(err)
