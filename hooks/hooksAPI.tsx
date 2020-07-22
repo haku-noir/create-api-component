54b09d8f35e3c3e672733c8a0921e39650876d58
haku-noir <hakunoir.00@gmail.com>
2020-07-23 03:51:15 +0900

    fix: propsとdispatchの型を修正

--- hooks/hooksAPI.tsx
+++ hooks/hooksAPI.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { Action } from 'redux';
+import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
 
-export const XXXAPI: React.FC<void> = () => {
-  const dispatch = useDispatch<Action>();
+export const XXXAPI: React.FC<{}> = () => {
+  const dispatch = useDispatch<Dispatch<Action>>();
 
   const state = useSelector<RootState, RootState>(state => state);
 
